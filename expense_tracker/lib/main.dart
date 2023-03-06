f3a4cae6d3456c4df83a4708b27e192253e5b43f
Rohit <108233235+TRohit20@users.noreply.github.com>
2023-03-06 20:06:24 +0530

    Setting up Expense Tracker app

---
+++ expense_tracker/lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Expense Planner')),
+      body: const Center(child: Text('Let\'s get started')),
+    );
+  }
+}
