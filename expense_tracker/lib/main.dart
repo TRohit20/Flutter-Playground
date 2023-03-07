2dff5854cb42292a7055269648924dbdd514b090
Rohit <108233235+TRohit20@users.noreply.github.com>
2023-03-07 13:16:27 +0530

    Understanding Row & Column Alignment

--- expense_tracker/lib/main.dart
+++ expense_tracker/lib/main.dart
@@ -1,17 +1,46 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Expense Tracker App',
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Expense Planner')),
-      body: const Center(child: Text('Let\'s get started')),
+      appBar: AppBar(
+        title: const Text('Expense Tracker App'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          Container(
+            width: double.infinity,
+            child: Card(
+              elevation: 10,
+              child: Container(
+                  width: 300, color: Colors.amber, child: Text('Charts')),
+            ),
+          ),
+          const Card(
+            elevation: 15,
+            child: Text('List of transactions'),
+          )
+        ],
+      ),
     );
   }
 }
