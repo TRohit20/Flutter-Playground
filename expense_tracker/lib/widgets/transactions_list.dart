27f4a24d7bcf159665c9ce7cc4d99b2a77ec4a17
Rohit <108233235+TRohit20@users.noreply.github.com>
2023-03-10 16:08:45 +0530

    Using ListView to make the list Scrollable and more efficient

--- expense_tracker/lib/widgets/transactions_list.dart
+++ expense_tracker/lib/widgets/transactions_list.dart
@@ -11,54 +11,53 @@ class TransactionList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // return Container(
-    //   height: 500,
-    //   child: SingleChildScrollView(
-    //     child: Column(
-    return Column(
-      // Using .map to transform objs to list of widgets and we use .list cz .map outputs iterables.
-      children: transactions.map((tx) {
-        return Card(
-          color: Colors.green[200],
-          // Using Columns inside rows and both inside a card
-          child: Row(
-            children: <Widget>[
-              Container(
-                margin: const EdgeInsets.all(15),
-                padding: const EdgeInsets.all(10),
-                decoration: BoxDecoration(
-                    border: Border.all(color: Colors.red, width: 2)),
-                child: Text(
-                  // In interpolation, when you are not just using a variable, but calling a property or more, then {} are used to wrap that expression
-                  '\$ ${tx.amount}',
-                  style: const TextStyle(
-                      color: Colors.black,
-                      fontSize: 15,
-                      fontWeight: FontWeight.bold),
-                ),
-              ),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Text(
-                    tx.title,
+    return Container(
+      height: 500,
+      child: ListView.builder(
+        itemBuilder: (context, index) {
+          return Card(
+            color: Colors.green[200],
+            // Using Columns inside rows and both inside a card
+            child: Row(
+              children: <Widget>[
+                Container(
+                  margin: const EdgeInsets.all(15),
+                  padding: const EdgeInsets.all(10),
+                  decoration: BoxDecoration(
+                      border: Border.all(color: Colors.red, width: 2)),
+                  child: Text(
+                    // In interpolation, when you are not just using a variable, but calling a property or more, then {} are used to wrap that expression
+                    '\$ ${transactions[index].amount}', // Accessing the specific transaction
                     style: const TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 18,
-                    ),
+                        color: Colors.black,
+                        fontSize: 15,
+                        fontWeight: FontWeight.bold),
                   ),
-                  Text(
-                    // DateFormat('dd-MM-yyyy').format(tx.date),
-                    DateFormat.yMMMd().format(tx.date),
-                    style: const TextStyle(color: Colors.grey, fontSize: 13),
-                  )
-                ],
-              )
-            ],
-          ),
-        );
-      }).toList(),
+                ),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      transactions[index].title,
+                      style: const TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 18,
+                      ),
+                    ),
+                    Text(
+                      // DateFormat('dd-MM-yyyy').format(tx.date),
+                      DateFormat.yMMMd().format(transactions[index].date),
+                      style: const TextStyle(color: Colors.grey, fontSize: 13),
+                    )
+                  ],
+                )
+              ],
+            ),
+          );
+        },
+        itemCount: transactions.length,
+      ),
     );
   }
 }
