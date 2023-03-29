4f50d116ab046e469b448d0215f196fea207397a
Rohit <108233235+TRohit20@users.noreply.github.com>
2023-03-29 22:47:03 +0530

    Using the LayoutBuilder Widget and Constraints
    Constraint is a object provided by flutter that allows you to dynamically calculate the sizes in height and width

--- expense_tracker/lib/widgets/ChartBar.dart
+++ expense_tracker/lib/widgets/ChartBar.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 class ChartBar extends StatelessWidget {
   final String label;
@@ -9,45 +10,51 @@ class ChartBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Container(
-            height:
-                20, // Wrapping with container and giving the height so that the text and size of Bars are aligned
-            child:
-                FittedBox(child: Text('\$ ${amountSpent.toStringAsFixed(0)}'))),
-        const SizedBox(
-          height: 5,
-        ), // Just to give some spacing
-        // Actual bar that is filled fractionally acc to data
-        Container(
-          height: 100,
-          width: 15,
-          // Stack is a widget that allows you to put things on each other as in Overlap
-          child: Stack(
-            // the 1st widget you add here will be the bottom most widget
-            children: [
-              Container(
-                decoration: BoxDecoration(
-                    border: Border.all(color: Colors.grey, width: 3),
-                    borderRadius: BorderRadius.circular(10),
-                    color: const Color.fromRGBO(220, 220, 220, 1)),
-              ),
-              FractionallySizedBox(
-                heightFactor: percentOfTotalAmountSpent,
-                child: Container(
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        return Column(
+          children: [
+            Container(
+                height: constraints.maxHeight *
+                    0.10, // Wrapping with container and giving the height so that the text and size of Bars are aligned
+                child: FittedBox(
+                    child: Text('\$ ${amountSpent.toStringAsFixed(0)}'))),
+            SizedBox(
+              height: constraints.maxHeight * 0.05,
+            ), // Just to give some spacing
+            // Actual bar that is filled fractionally acc to data
+            Container(
+              height: constraints.maxHeight * 0.7,
+              width: 15,
+              // Stack is a widget that allows you to put things on each other as in Overlap
+              child: Stack(
+                // the 1st widget you add here will be the bottom most widget
+                children: [
+                  Container(
                     decoration: BoxDecoration(
-                        color: Colors.green.shade300,
-                        borderRadius: BorderRadius.circular(10))),
-              )
-            ],
-          ),
-        ),
-        const SizedBox(
-          height: 5,
-        ),
-        Text(label)
-      ],
+                        border: Border.all(color: Colors.grey, width: 3),
+                        borderRadius: BorderRadius.circular(10),
+                        color: const Color.fromRGBO(220, 220, 220, 1)),
+                  ),
+                  FractionallySizedBox(
+                    heightFactor: percentOfTotalAmountSpent,
+                    child: Container(
+                        decoration: BoxDecoration(
+                            color: Colors.green.shade300,
+                            borderRadius: BorderRadius.circular(10))),
+                  )
+                ],
+              ),
+            ),
+            SizedBox(
+              height: constraints.maxHeight * 0.05,
+            ),
+            Container(
+                height: constraints.maxHeight * 0.10,
+                child: FittedBox(child: Text(label)))
+          ],
+        );
+      },
     );
   }
 }
