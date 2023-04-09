54bbe9d200d25decb1369bf24a49506a340cd90c
Rohit <108233235+TRohit20@users.noreply.github.com>
2023-04-09 23:13:56 +0530

    Meals App

---
+++ ziggy_a_recipes_app/android/app/src/main/kotlin/com/example/ziggy_a_recipes_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.ziggy_a_recipes_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ ziggy_a_recipes_app/lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Ziggy Recipes',
+      theme: ThemeData(primarySwatch: Colors.deepOrange),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+          title: const Text(
+        'Ziggy',
+        style: TextStyle(fontWeight: FontWeight.bold),
+      )),
+      body: Center(
+        child: Text('This is the Home Page'),
+      ),
+    );
+  }
+}
