4c43bbcfd109fee75050a436e8988d49d45fae36
Rohit <108233235+TRohit20@users.noreply.github.com>
2023-04-11 13:02:17 +0530

    Registering a Screen as Main Screen or other Options

--- ziggy_a_recipes_app/lib/categories_screen.dart
+++ ziggy_a_recipes_app/lib/categories_screen.dart
@@ -5,15 +5,22 @@ import 'package:ziggy_a_recipes_app/dummy_data.dart';
 class CategoriesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return GridView(
-        gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
-            maxCrossAxisExtent: 150,
-            childAspectRatio: 3 / 2,
-            mainAxisSpacing: 20,
-            crossAxisSpacing: 20),
-        children: dummyCategories
-            .map((categoryData) =>
-                CategoryItem(categoryData.title, categoryData.color))
-            .toList());
+    return Scaffold(
+      appBar: AppBar(
+          title: const Text(
+        'Ziggy',
+        style: TextStyle(fontWeight: FontWeight.bold),
+      )),
+      body: GridView(
+          gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+              maxCrossAxisExtent: 150,
+              childAspectRatio: 3 / 2,
+              mainAxisSpacing: 20,
+              crossAxisSpacing: 20),
+          children: dummyCategories
+              .map((categoryData) =>
+                  CategoryItem(categoryData.title, categoryData.color))
+              .toList()),
+    );
   }
 }
--- ziggy_a_recipes_app/lib/main.dart
+++ ziggy_a_recipes_app/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:ziggy_a_recipes_app/categories_screen.dart';
 
 void main() => runApp(MyApp());
 
@@ -8,7 +9,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Ziggy Recipes',
       theme: ThemeData(primarySwatch: Colors.deepOrange),
-      home: MyHomePage(),
+      home: CategoriesScreen(),
     );
   }
 }
