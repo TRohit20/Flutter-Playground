7ffc39383d544528f6c9340798a0671e06d8b5ce
Rohit <108233235+TRohit20@users.noreply.github.com>
2023-04-17 13:21:51 +0530

    Enabling Dark Theme

--- expense_tracker_version_2/lib/main.dart
+++ expense_tracker_version_2/lib/main.dart
@@ -2,9 +2,26 @@ import 'package:flutter/material.dart';
 import './widgets/expenses.dart';
 
 var kColorScheme =
-    ColorScheme.fromSeed(seedColor: Color.fromARGB(238, 11, 194, 185));
+    ColorScheme.fromSeed(seedColor: const Color.fromARGB(238, 11, 194, 185));
+
+var kDarkColorScheme = ColorScheme.fromSeed(
+    brightness: Brightness.dark, seedColor: Color.fromARGB(238, 89, 90, 116));
 void main() {
   runApp(MaterialApp(
+    themeMode: ThemeMode.system,
+    darkTheme: ThemeData.dark().copyWith(
+      useMaterial3: true,
+      colorScheme: kDarkColorScheme,
+      cardTheme: const CardTheme().copyWith(
+        color: kDarkColorScheme.secondaryContainer,
+        margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+        elevation: 15,
+      ),
+      elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+              backgroundColor: kDarkColorScheme.primaryContainer,
+              foregroundColor: kDarkColorScheme.onPrimaryContainer)),
+    ),
     theme: ThemeData().copyWith(
         useMaterial3: true,
         colorScheme: kColorScheme,
